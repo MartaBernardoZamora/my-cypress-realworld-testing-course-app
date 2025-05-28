f7d835d86e4d42c00b68e54dc57edf2116d82151
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-05-28 10:19:46 +0200

    feat: theoretical part code

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,14 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io")
+describe("home page", () => {
+  it('the h1 contains the correct text', () => {
+    cy.visit('http://localhost:3000')
+    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+  })
+  it.only("the features on the homepage are correct", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(0).contains("4 Courses")
+  })
+  it.only("the features on the homepage are correct with regex", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(0).contains(/4 courses/i)
   })
 })
