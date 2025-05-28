75c5c0e273d2abdf9de20143e09d1311f37aec35
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-05-28 15:00:45 +0200

    fix: set desktop viewport and wait for Next.js mount point to be visible to config test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,11 @@
 /// <reference types="cypress" />
+const originalVisit = cy.visit
+
+Cypress.Commands.overwrite('visit', (originalFn, ...args) => {
+  originalFn(...args)
+  cy.get('#__next', { timeout: 10000 }).should('be.visible')
+})
+
 declare namespace Cypress {
   interface Chainable {
     getByData(dataTestAttribute: string): Chainable<JQuery<HTMLElement>>
