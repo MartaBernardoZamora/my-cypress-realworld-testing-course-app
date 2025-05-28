e45f98355638eccacee857e3ba983c6e9f07f8b8
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-05-28 10:36:14 +0200

    feat: practice1 completed

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,12 +3,24 @@ describe("home page", () => {
     cy.visit('http://localhost:3000')
     cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
   })
-  it.only("the features on the homepage are correct", () => {
+  it("the features on the homepage are correct", () => {
     cy.visit("http://localhost:3000")
     cy.get("dt").eq(0).contains("4 Courses")
   })
-  it.only("the features on the homepage are correct with regex", () => {
+  it("the features on the homepage are correct with regex", () => {
     cy.visit("http://localhost:3000")
     cy.get("dt").eq(0).contains(/4 courses/i)
+  })
+  it.only("the second feature on the homepage are correct", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(1).contains('25+ Lessons')
+  })
+  it.only("the second feature on the homepage are correct with regex", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(1).contains(/25\+\s*lessons/i)
+  })
+  it.only("the third feature on the homepage are correct with regex", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(2).contains(/free and open source/i)
   })
 })
