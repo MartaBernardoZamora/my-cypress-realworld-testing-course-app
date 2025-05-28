6b1e747e954e58a3058b86e67c5c50d1c28cad86
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-05-28 14:40:26 +0200

    feat: custom command theoretical part code

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -2,8 +2,9 @@ describe("home page", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000")
   })
-  it('the h1 contains the correct text', () => {
-    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+  it.only('the h1 contains the correct text', () => {
+    //cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+    cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
   })
   it("the features on the homepage are correct", () => {
     cy.get("dt").eq(0).contains("4 Courses")
@@ -11,13 +12,13 @@ describe("home page", () => {
   it("the features on the homepage are correct with regex", () => {
     cy.get("dt").eq(0).contains(/4 courses/i)
   })
-  it.only("the second feature on the homepage are correct", () => {
+  it("the second feature on the homepage are correct", () => {
     cy.get("dt").eq(1).contains('25+ Lessons')
   })
-  it.only("the second feature on the homepage are correct with regex", () => {
+  it("the second feature on the homepage are correct with regex", () => {
     cy.get("dt").eq(1).contains(/25\+\s*lessons/i)
   })
-  it.only("the third feature on the homepage are correct with regex", () => {
+  it("the third feature on the homepage are correct with regex", () => {
     cy.get("dt").eq(2).contains(/free and open source/i)
   })
 })
