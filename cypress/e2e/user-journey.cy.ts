f60fd30a04d158fc57413c8321d95c99d779c219
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-05-30 13:37:14 +0200

    feat: practice 4 completed

--- cypress/e2e/user-journey.cy.ts
+++ cypress/e2e/user-journey.cy.ts
@@ -11,5 +11,14 @@ describe("User Journey", () => {
     cy.getByData("challenge-answer-0").click()
     cy.getByData("next-lesson-button").should("exist").click()
     cy.location("pathname").should("eq", "/testing-your-first-application/installing-cypress-and-writing-our-first-test")
+
+    cy.getByData("challenge-answer-0").click()
+    cy.getByData("next-lesson-button").should("exist").click()
+    cy.location("pathname").should("eq", "/testing-your-first-application/setting-up-data-before-each-test")
+
+    cy.getByData("challenge-answer-0").click()
+    cy.getByData("next-lesson-button").should("exist").contains("Complete Course").click()
+    cy.location("pathname").should("eq", "/")
+
   })
 })
