d0464f4ebe15ea379cad06329391bca119f5d7d3
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-05-30 09:24:45 +0200

    feat: theoretical part context and location code

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -2,15 +2,24 @@ describe("home page", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000")
   })
-  it('the h1 contains the correct text', () => {
-    //cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
-    cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+  context("Hero section", () => {
+    it('the h1 contains the correct text', () => {
+      //cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+      cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+    })
+    it("the features on the homepage are correct", () => {
+      //cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(0).contains(/4 courses/i)
+      cy.get("dt").eq(1).contains('25+ Lessons')
+      //cy.get("dt").eq(1).contains(/25\+\s*lessons/i)
+      cy.get("dt").eq(2).contains(/free and open source/i)
+    })
   })
-  it("the features on the homepage are correct", () => {
-    //cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(0).contains(/4 courses/i)
-    cy.get("dt").eq(1).contains('25+ Lessons')
-    //cy.get("dt").eq(1).contains(/25\+\s*lessons/i)
-    cy.get("dt").eq(2).contains(/free and open source/i)
+
+  context.only("Courses section", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    })
   })
 })
